1907a0f1aa5706a57bb0a75ed659496167052049
Richard Mountain <web@richardmountain.me>
2016-11-11 12:26:48 +0000

    Updated page title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
+        <title><?php the_title(); ?> | <?php print get_bloginfo(); ?> - <?php print get_bloginfo('description'); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
