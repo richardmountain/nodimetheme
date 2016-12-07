21fc4ebb2e9abd1197dec9cf1b57cc02f4737600
Richard Mountain <web@richardmountain.me>
2016-12-07 14:26:17 +0000

    Added new theme styling

--- front-page.php
+++ front-page.php
@@ -1,55 +1,42 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _tk
+ */
 
-<div id="main">
-
-	<div id="content" class="container">
-
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-			<div class="row post">
-
-				<div class="col-md-7  col-md-offset-3  ">
-
-					<div class="row">
-
-						<div class="col-md-12  metadata">
-
-							<h4><?php the_time('F j, Y'); ?> <a href="<?php print get_post_permalink( $post->ID ); ?>#respond">Leave a Comment</a></h4>
-
-						</div>
-
-					</div>
-
-					<div class="row">
-
-						<div class="col-md-12">
-							
-							<h1><a href="<?php print get_post_permalink( $post->ID ); ?>"><?php the_title(); ?></a></h1>
-
-						</div>
-
-					</div>
-
-					<div class="row">
-
-						<div class="col-md-12">
-							
-							<p><?php the_content(__('(more...)')); ?></p>
-							
-						</div>
-
-					</div>		
-
-				</div>
-
-			</div>
-
-			<?php endwhile; else: ?>
-
-			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-
-	</div>
+get_header(); ?>
 	
-</div>
+	<?php // Get blog posts
+		$temp = $wp_query; $wp_query= null;
+		$wp_query = new WP_Query(); $wp_query->query('showposts=6');
+	?>
+	<h2>Latest Posts</h2>
+	<?php if ( $wp_query->have_posts() ) : ?>
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+
+			<?php
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to overload this in a child theme then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content-front', get_post_format() );
+			?>
+
+		<?php endwhile; ?>
+
+	<?php else : ?>
+
+		<?php get_template_part( 'no-results', 'index' ); ?>
+
+	<?php endif; ?>
 
 <?php get_footer(); ?>
